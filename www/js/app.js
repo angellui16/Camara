bfbdc3d842816e75ec0cd8e2848f65fdf77d2535
angellui16 <angel092479@gmail.com>
2016-07-15 12:45:41 -0600

    Camara

---
+++ www/js/app.js
@@ -0,0 +1,30 @@
+var deviceReadyDeferred = $.Deferred();
+var jqmReadyDeferred = $.Deferred();
+
+$(document).on("deviceready", function() {
+  deviceReadyDeferred.resolve();
+});
+
+$(document).on("mobileinit", function () {
+  jqmReadyDeferred.resolve();
+});
+
+$.when(deviceReadyDeferred, jqmReadyDeferred).then(init);
+
+function init() {
+}
+
+function getPhoto(){
+    navigator.camera.getPicture(onSuccess, onFail, { quality: 50,
+    destinationType: Camera.DestinationType.DATA_URL
+    });
+
+    function onSuccess(imageData) {
+        var image = document.getElementById('cameraImage');
+        image.src = "data:image/jpeg;base64," + imageData;
+    }
+
+    function onFail(message) {
+        alert('Failed because: ' + message);
+    }
+}
